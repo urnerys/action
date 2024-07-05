4e797919a223cea2f3761ae07e77f92d5bad52d5
Tomáš Čarnecký <tomc@caurea.org>
2024-07-05 17:53:07 +0200

    .

--- createBuild/index.mjs
+++ createBuild/index.mjs
@@ -1,9 +1,9 @@
-const { URNERYS, GITHUB_RUN_ID, GITHUB_SHA } = process.env
+const { URNERYS, GITHUB_RUN_ID, GITHUB_REPOSITORY, GITHUB_SHA } = process.env
 
 const res = await fetch(`https://${URNERYS}/rpc/createBuild`, {
   method: "POST",
   body: JSON.stringify({
-    project: "stories",
+    project: GITHUB_REPOSITORY.split('/')[1],
     build: GITHUB_RUN_ID,
     commit: GITHUB_SHA
   }),
