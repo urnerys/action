6ef92e52485615b022ec884021b7ca4c02bff459
Tomáš Čarnecký <tomc@caurea.org>
2024-07-05 18:36:54 +0200

    .

---
+++ setBaseline/index.mjs
@@ -0,0 +1,13 @@
+const { URNERYS, GITHUB_RUN_ID, GITHUB_REPOSITORY, GITHUB_SHA } = process.env
+
+const res = await fetch(`https://${URNERYS}/rpc/setBaseline`, {
+  method: "POST",
+  body: JSON.stringify({
+    project: GITHUB_REPOSITORY.split('/')[1],
+    baseline: "production"
+    build: GITHUB_RUN_ID,
+  }),
+});
+
+console.log("setBaseline", res.status);
+console.log(await res.text());
