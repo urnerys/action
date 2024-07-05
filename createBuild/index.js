a4f6ed9addbb47715d0982c5d444116c4efb320c
Tomáš Čarnecký <tomc@caurea.org>
2024-07-05 17:44:46 +0200

    Create index.js

---
+++ createBuild/index.js
@@ -0,0 +1,13 @@
+const { URNERYS, GITHUB_RUN_ID, GITHUB_SHA } = process.env
+
+const res = await fetch(`https://${URNERYS}/rpc/createBuild", {
+  method: "POST",
+  body: JSON.stringify({
+    project: "stories",
+    build: GITHUB_RUN_ID,
+    commit: GITHUB_SHA
+  }),
+});
+
+console.log("createBuild", res.status);
+console.log(await res.text());
